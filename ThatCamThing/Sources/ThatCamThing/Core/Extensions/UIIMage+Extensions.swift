1a25e5a378dd1e0d45f530aa59007e5d4c9fb65a
angel zambrano <angeldzambrano99@gmail.com>
2025-07-01 19:07:00 -0700

    updated extensions

---
+++ ThatCamThing/Sources/ThatCamThing/Core/Extensions/UIIMage+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  UIIMage+Extensions.swift
+//  ThatCamThing
+//
+//  Created by angel zambrano on 7/1/25.
+//
+
+
+import SwiftUI
+
+// MARK: - UI Extensions
+
+extension UIImage {
+    func mirrored() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: size)
+        return renderer.image { context in
+            context.cgContext.translateBy(x: size.width, y: 0)
+            context.cgContext.scaleBy(x: -1, y: 1)
+            draw(at: .zero)
+        }
+    }
+}
