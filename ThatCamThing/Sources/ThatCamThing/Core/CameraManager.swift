a562f9cd512c6439d23afd89414ef3ba13e292ec
angel zambrano <angeldzambrano99@gmail.com>
2025-07-01 19:13:16 -0700

    added camera manager

---
+++ ThatCamThing/Sources/ThatCamThing/Core/CameraManager.swift
@@ -0,0 +1,121 @@
+//
+//  CameraManager.swift
+//  ThatCamThing
+//
+//  Created by angel zambrano on 7/1/25.
+//
+
+import Foundation
+
+import SwiftUI
+import AVKit
+
+// MARK: - Main Camera Manager
+class CameraManager: NSObject, CameraManaging {
+    
+    // MARK: - Published Properties
+    @Published var attributes: CameraManagerAttributes = .init() {
+        didSet {
+            configurationManager.handleAttributeChanges(oldValue: oldValue, newValue: attributes)
+        }
+    }
+    
+    @Published var isSessionRunning = false
+    @Published var isPaused = false
+    @Published var isShowingAlert = false
+    @Published var alertMessage = ""
+    @Published var capturedImage: UIImage?
+    @Published var isCapturing: Bool = false
+
+    // MARK: - Callbacks
+    var onImageCaptured: ((UIImage) -> Void)?
+    
+    // MARK: - Core Components
+    let session = AVCaptureSession()
+    
+    // MARK: - Managers
+    lazy var deviceManager = CameraDeviceManager(session: session, cameraManager: self)
+    lazy var sessionManager = CameraSessionManager(session: session, cameraManager: self)
+    lazy var permissionManager = CameraPermissionManager(cameraManager: self)
+    lazy var photoCaptureManager = PhotoCaptureManager(cameraManager: self)
+    lazy var configurationManager = CameraConfigurationManager(cameraManager: self)
+    
+    // MARK: - Initialization
+    
+    init(initialAttributes: CameraManagerAttributes = .init()) {
+        self.attributes = initialAttributes
+        super.init()
+        
+        setupCamera()
+        sessionManager.setupSessionObservers()
+    }
+    
+    convenience init(
+        cameraPosition: CameraPosition = .back,
+        resolution: AVCaptureSession.Preset = .hd1920x1080,
+        flashMode: CameraFlashMode = .off,
+        frameRate: Int32 = 30,
+        zoomFactor: CGFloat = 1.0
+    ) {
+        var attributes = CameraManagerAttributes()
+        attributes.cameraPosition = cameraPosition
+        attributes.resolution = resolution
+        attributes.flashMode = flashMode
+        attributes.frameRate = frameRate
+        attributes.zoomFactor = zoomFactor
+        
+        self.init(initialAttributes: attributes)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    // MARK: - Setup
+    private func setupCamera() {
+        session.sessionPreset = attributes.resolution
+        
+        guard deviceManager.setupInitialDevice(
+            position: attributes.cameraPosition,
+            frameRate: attributes.frameRate,
+            zoomFactor: attributes.zoomFactor
+        ) else {
+            attributes.error = .cannotSetupInput
+            return
+        }
+        
+        guard photoCaptureManager.setupPhotoOutput(session: session) else {
+            attributes.error = .cannotSetupOutput
+            return
+        }
+    }
+    
+    // MARK: - Public Interface
+    func requestPermissions() {
+        permissionManager.requestPermissions()
+    }
+    
+    func togglePause() {
+        if isPaused {
+            sessionManager.resumeSession()
+        } else {
+            sessionManager.pauseSession()
+        }
+    }
+    
+    func capturePhoto() {
+        photoCaptureManager.capturePhoto(flashMode: attributes.flashMode)
+    }
+    
+    func clearCaptureImage() {
+        capturedImage = nil
+        attributes.capturedMedia = nil
+    }
+    
+    func showAlert(message: String) {
+        DispatchQueue.main.async {
+            self.alertMessage = message
+            self.isShowingAlert = true
+        }
+    }
+}
---
+++ ThatCamThing/Sources/ThatCamThing/Managers/PhotoCaptureManager.swift
@@ -0,0 +1,102 @@
+//
+//  PhotoCaptureManager.swift
+//  ThatCamThing
+//
+//  Created by angel zambrano on 7/1/25.
+//
+
+import Foundation
+
+
+import Foundation
+import AVKit
+import SwiftUI
+import Photos
+
+// MARK: - Photo Capture Management
+
+class PhotoCaptureManager: NSObject {
+    private weak var cameraManager: CameraManager?
+    var photoOutput: AVCapturePhotoOutput?
+    private let imageProcessingQueue = DispatchQueue(label: "com.camera.imageProcessing", qos: .userInitiated)
+
+    
+    init(cameraManager: CameraManager) {
+        self.cameraManager = cameraManager
+        super.init()
+    }
+    
+    func setupPhotoOutput(session: AVCaptureSession) -> Bool {
+        photoOutput = AVCapturePhotoOutput()
+        
+        guard let photoOutput = photoOutput else { return false }
+        
+        if session.canAddOutput(photoOutput) {
+            session.addOutput(photoOutput)
+            return true
+        }
+        
+        return false
+    }
+    
+    func capturePhoto(flashMode: CameraFlashMode) {
+        guard let photoOutput = photoOutput,
+              let cameraManager = cameraManager,
+              !cameraManager.isPaused else { return }
+        
+        let settings = AVCapturePhotoSettings()
+        
+        switch flashMode {
+        case .off:
+            settings.flashMode = .off
+        case .on:
+            settings.flashMode = .on
+        case .auto:
+            settings.flashMode = .auto
+        }
+        
+        photoOutput.capturePhoto(with: settings, delegate: self)
+    }
+}
+
+extension PhotoCaptureManager: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        
+        // Move heavy processing to background thread
+        imageProcessingQueue.async { [weak self] in
+            guard let self = self else { return }
+            
+            // Handle error case
+            guard error == nil else {
+                DispatchQueue.main.async {
+                    self.cameraManager?.showAlert(message: "Failed to capture photo: \(error?.localizedDescription ?? "Unknown error")")
+                    self.cameraManager?.isCapturing = false
+                }
+                return
+            }
+            
+            guard let imageData = photo.fileDataRepresentation(),
+                  let image = UIImage(data: imageData) else {
+                DispatchQueue.main.async {
+                    self.cameraManager?.showAlert(message: "Failed to capture photo")
+                    self.cameraManager?.isCapturing = false
+                }
+                return
+            }
+            
+            // Apply mirroring if needed (this is expensive)
+            let finalImage = self.cameraManager?.attributes.mirrorOutput == true ? image.mirrored() : image
+            
+            let metadata = photo.metadata
+            let cameraMedia = CameraMedia(image: finalImage, metadata: metadata, timestamp: Date())
+            
+            // Update UI on main thread
+            DispatchQueue.main.async {
+                self.cameraManager?.capturedImage = finalImage
+                self.cameraManager?.attributes.capturedMedia = cameraMedia
+                self.cameraManager?.isCapturing = false // Re-enable capture
+                self.cameraManager?.onImageCaptured?(finalImage)
+            }
+        }
+    }
+}
