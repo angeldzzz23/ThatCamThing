04c595ab955b80fe4079b399d16a48300927a3ce
angel zambrano <angeldzambrano99@gmail.com>
2025-07-01 19:20:44 -0700

    updated

---
+++ ThatCamThing/Sources/ThatCamThing/Views/Preview/VideoPreviewView.swift
@@ -0,0 +1,35 @@
+//
+//  VideoPreviewView.swift
+//  ThatCamThing
+//
+//  Created by angel zambrano on 7/1/25.
+//
+
+
+import Foundation
+import AVKit
+import SwiftUI
+import Photos
+
+
+class VideoPreviewView: UIView {
+    var session: AVCaptureSession? {
+        didSet {
+            guard let session = session else { return }
+            videoPreviewLayer.session = session
+        }
+    }
+    
+    override class var layerClass: AnyClass {
+        AVCaptureVideoPreviewLayer.self
+    }
+    
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        layer as! AVCaptureVideoPreviewLayer
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        videoPreviewLayer.videoGravity = .resizeAspectFill
+    }
+}
