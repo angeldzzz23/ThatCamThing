699a6e91f655f96f78a5e0cc8653870a6fba8eaa
angel zambrano <angeldzambrano99@gmail.com>
2025-07-01 19:08:41 -0700

    camera configuration manager

---
+++ ThatCamThing/Sources/ThatCamThing/Managers/CameraConfigurationManager.swift
@@ -0,0 +1,42 @@
+//
+//  CameraConfigurationManager.swift
+//  ThatCamThing
+//
+//  Created by angel zambrano on 7/1/25.
+//
+
+
+import Foundation
+import AVKit
+import SwiftUI
+import Photos
+
+// MARK: - Configuration Management
+class CameraConfigurationManager {
+    
+    private weak var cameraManager: CameraManager?
+    
+    init(cameraManager: CameraManager) {
+        self.cameraManager = cameraManager
+    }
+    
+    func handleAttributeChanges(oldValue: CameraManagerAttributes, newValue: CameraManagerAttributes) {
+        
+        if oldValue.cameraPosition != newValue.cameraPosition {
+            cameraManager?.deviceManager.switchCamera(to: newValue.cameraPosition)
+        }
+        
+        if oldValue.resolution != newValue.resolution {
+            cameraManager?.sessionManager.updateResolution(newValue.resolution)
+        }
+        
+        if oldValue.zoomFactor != newValue.zoomFactor {
+            cameraManager?.deviceManager.updateZoom(newValue.zoomFactor)
+        }
+        
+        if oldValue.frameRate != newValue.frameRate {
+            cameraManager?.deviceManager.updateFrameRate(newValue.frameRate)
+        }
+    }
+    
+}
