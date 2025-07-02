bcec62e692fe7ba1132c308fb623db35282fdeb5
angel zambrano <angeldzambrano99@gmail.com>
2025-07-01 19:20:31 -0700

    updated camera preview

---
+++ ThatCamThing/Sources/ThatCamThing/Views/Preview/CameraPreview.swift
@@ -0,0 +1,28 @@
+//
+//  CameraPreview.swift
+//  ThatCamThing
+//
+//  Created by angel zambrano on 7/1/25.
+//
+
+
+import Foundation
+import AVKit
+import SwiftUI
+import Photos
+
+// MARK: - Camera Preview UIViewRepresentable
+
+struct CameraPreview: UIViewRepresentable {
+    let session: AVCaptureSession
+    
+    func makeUIView(context: Context) -> VideoPreviewView {
+        let view = VideoPreviewView()
+        view.session = session
+        return view
+    }
+    
+    func updateUIView(_ uiView: VideoPreviewView, context: Context) {
+        // Update if needed
+    }
+}
