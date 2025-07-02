2085ec1ee028542c409ca1c1901e7cd2ea812e99
angel zambrano <angeldzambrano99@gmail.com>
2025-07-01 18:48:39 -0700

    added camera package

---
+++ ThatCamThing/Sources/ThatCamThing/PracticeButton.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView.swift
+//  ThatCamThing
+//
+//  Created by angel zambrano on 7/1/25.
+//
+
+import SwiftUI
+
+struct PracticeButton: View {
+    var body: some View {
+        Button("Button") {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    PracticeButton()
+}
